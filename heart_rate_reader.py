eb7b696ac905cc347bba001d763e9c498cb8d800
tm232 <tm232@duke.edu>
2018-10-23 13:47:07 -0400

    set up travis

--- heart_rate_reader.py
+++ heart_rate_reader.py
@@ -3,29 +3,42 @@ import csv
 
 def main():
     # print("Main")
-    parse_data('test_data/test_data1.csv')
-
+    data = parse_data('test_data/test_data1.csv')
+    print(data)
+    calculate_values(data)
 
 def parse_data(file):
     # print("reading")
+    time = [];
     time_data = [];
+    run_avg = [];
+    ctr = 0;
     try:
         with open(file) as csvfile:
             reader = csv.reader(csvfile)
             for row in reader:
                 try:
-                    time_data.append((float(row[0]), float(row[1])))
+                    time.append(float(row[0]))
+                    time_data.append(float(row[1]))
+                    if ctr >= 4:
+                        run_avg.append((time_data[ctr]
+                                       + time_data[ctr-1] + time_data[ctr-2]
+                                       + time_data[ctr-3] + time_data[ctr-4])/5)
+                    ctr = ctr + 1
                 except ValueError:
                     print("could not convert data to a float")
     except OSError:
         print("cannot open")
     else:
-        print(time_data)
-        return time_data
+        # print((time, run_avg))
+        return time, run_avg
 
 
 def calculate_values(data):
     print("Calculating")
+    vMax = max(data[1])
+    print(vMax)
+
 
 
 if __name__ == "__main__":
