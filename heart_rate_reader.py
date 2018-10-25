7bad7a4d0409b5b3b007f6376a18083abbb16a03
tm232 <tm232@duke.edu>
2018-10-25 16:31:23 -0400

    added more tests, better peak finding algorithm

--- heart_rate_reader.py
+++ heart_rate_reader.py
@@ -2,8 +2,8 @@ import csv
 
 
 def main():
-    data = parse_data('test_data/test_data15.csv')
-    calculate_values(data[0], data[1], data[2])
+    data = parse_data('test_data/test_data1.csv')
+    calculate_values(data[0], data[1])
 
 
 def parse_data(file):
@@ -43,36 +43,38 @@ def parse_data(file):
                     ctr = ctr + 1
                 except ValueError:
                     print("could not convert data to a float")
+            # fill in unaveraged values of times_avg to get all times
+            times_avg.insert(0, time[1])
+            times_avg.insert(0, time[0])
+            times_avg.insert(len(times_avg), time[len(time)-2])
+            times_avg.insert(len(times_avg), time[len(time)-1])
+            # fill in unaveraged values of time_data to get all voltages
+            run_avg.insert(0, time_data[1])
+            run_avg.insert(0, time_data[0])
+            run_avg.insert(len(run_avg), time_data[len(time_data)-2])
+            run_avg.insert(len(run_avg), time_data[len(time_data)-1])
     except IOError as e:
         print(e)
         raise
     else:
-        # print((time, run_avg))
+        print((time, run_avg))
         return times_avg, run_avg, time, time_data
 
 
-def calculate_values(times, values, times_all = []):
+def calculate_values(times, values):
     """
     Calculates metrics:
         mean_hr_bpm, voltage_extremes (max, min), duration of data,
         num_beats, beats [times]
     :param times: [float] array of times corresponding with voltage values
     :param values: [float] array of voltage values corresponding with times
-    :param times_all: (optional) if times list does not cover the entire duration
-        of the ecg strip
-    :return: metrics dictionary
+    :return: metrics dictionary, message (stating if metrics are weird)
     """
     peak_times = find_peaks(times, values)
-    duration = 0
-    if len(times_all) == 0:
-        # times_all not entered; use the duration from times list
-        duration = max(times)
-    else:
-        duration = max(times_all)
     metrics = {
-        "mean_hr_bpm": round(len(peak_times)/max(times_all)*60),
+        "mean_hr_bpm": round(len(peak_times)/max(times)*60),
         "voltage_extremes": (round(max(values), 3), round(min(values), 3)),
-        "duration": duration,
+        "duration": max(times),
         "num_beats": len(peak_times),
         "beats": peak_times
     }
@@ -95,7 +97,7 @@ def find_peaks(times, values):
         :param values: [float] list of voltage values corresponding with times
 
     Returns:
-        :return: peak_times [float]: list of times corresponding to local maxima
+        :return: peak_times [float] list of times corresponding to local maxima
             (voltage peaks)
     """
     v_max = max(values)
@@ -104,20 +106,15 @@ def find_peaks(times, values):
     print(thresh)
     peak_times = []
     peak_vals = []
-    calc_peak_times = []
-    calc_peak_vals = []
+    depolar = False
     for i in range(1, len(values)):
         if values[i] > thresh:
-            calc_peak_vals.append(values[i])
-            calc_peak_times.append(times[i])
-        elif (values[i-1] > thresh) and (values[i] <= thresh):
-            # just after peak
-            ind = calc_peak_vals.index(max(calc_peak_vals))
-            peak_times.append(round(calc_peak_times[ind], 3))
-            peak_vals.append(round(calc_peak_vals[ind], 3))
-            # reset lists to empty
-            calc_peak_times = []
-            calc_peak_vals = []
+            if values[i] < values[i-1] and not depolar:
+                peak_times.append(times[i-1])
+                peak_vals.append(values[i-1])
+                depolar = True
+        else:
+            depolar = False
 
     # print(peak_times)
     # print(peak_vals)
